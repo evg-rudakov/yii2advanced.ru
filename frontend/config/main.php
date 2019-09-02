5c418fde3d1613ce3f963c7ec428a70e8262908f
AnaninAN <ananinpgu@mail.ru>
2019-09-02 21:11:40 +0300

    Нстройка ЧПУ

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
