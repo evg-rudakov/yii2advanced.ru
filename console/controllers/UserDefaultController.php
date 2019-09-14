6b7a91b5d3493ef58db5501e8e904221013b1115
AnaninAN <ananinpgu@mail.ru>
2019-09-14 07:14:05 +0300

    Переписал процедуру добавление роли пользователю через authManager

--- console/controllers/UserDefaultController.php
+++ console/controllers/UserDefaultController.php
@@ -2,7 +2,7 @@
 
 namespace console\controllers;
 
-use common\models\AuthAssignment;
+use Yii;
 use yii\console\Controller;
 use common\models\User;
 
@@ -18,16 +18,9 @@ class UserDefaultController extends Controller
             $user->generateAuthKey();
             if ($user->save()) {
                 echo "{$usr} created! \r\n";
-                $auth = new AuthAssignment();
-                $auth->item_name = $authusr;
-                $auth->user_id = (string) $user->id;
-                if ($auth->save()) {
-                    echo "{$usr} assignment '{$authusr}' \r\n";
-                } else {
-                    var_dump($auth->item_name);
-                    var_dump($auth->errors);
-                    die();
-                }
+                $role = Yii::$app->authManager->getRole($authusr);
+                Yii::$app->authManager->assign($role, (string) $user->id);
+                echo "{$usr} assignment '{$authusr}' \r\n";
             } else {
                 var_dump($user->id);
                 var_dump($user->errors);
